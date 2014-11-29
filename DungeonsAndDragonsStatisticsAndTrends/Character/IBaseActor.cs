a30312bfefe4a2b5e46db0c44d4f660ded6008f3
alfonsopuga <alfonsopuga@live.com>
2014-11-29 11:35:43 -0800

    Master -2-Fixed IBaseMonster

---
+++ DungeonsAndDragonsStatisticsAndTrends/Character/IBaseActor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actor
+{
+    interface IBaseActor
+    {
+        /// <summary>
+        /// Gets the Name of the Actor
+        /// </summary>
+        string Name { get;}
+        /// <summary>
+        /// Gets the AC of the Actor
+        /// </summary>
+        int AC { get; }
+        /// <summary>
+        /// Gets the Strength of the Actor
+        /// </summary>
+        int Strength { get; }
+        /// <summary>
+        /// Gets the Dexterity of the Actor
+        /// </summary>
+        int Dexterity { get; }
+        /// <summary>
+        /// Gets the Constitution of the Actor
+        /// </summary>
+        int Constitution { get; }
+        /// <summary>
+        /// Gets the Intelligence of the Actor
+        /// </summary>
+        int Intelligence { get; }
+        /// <summary>
+        /// Gets the Wisdom of the Actor
+        /// </summary>
+        int Wisdom { get; }
+        /// <summary>
+        /// Gets the Charisma of the Actor
+        /// </summary>
+        int Charisma { get; }
+        /// <summary>
+        /// Gets the StrengthMod of the Actor
+        /// </summary>
+        int StrengthMod { get; }
+        /// <summary>
+        /// Gets the DexterityMod of the Actor
+        /// </summary>
+        int DexterityMod { get; }
+        /// <summary>
+        /// Gets the ConstitutionMod of the Actor
+        /// </summary>
+        int ConstitutionMod { get; }
+        /// <summary>
+        /// Gets the IntelligenceMod of the Actor
+        /// </summary>
+        int IntelligenceMod { get; }
+        /// <summary>
+        /// Gets the WisdomMod of the Actor
+        /// </summary>
+        int WisdomMod { get; }
+        /// <summary>
+        /// Gets the CharismaMod of the Actor
+        /// </summary>
+        int CharismaMod { get; }
+
+    }
+}
--- DungeonsAndDragonsStatisticsAndTrends/Character/Monsters/Goblin.cs
+++ DungeonsAndDragonsStatisticsAndTrends/Character/Monsters/Goblin.cs
@@ -8,24 +8,32 @@ namespace Monster
 {
     class Goblin : IBaseMonster
     {
+        public string Name{ get; private set;}
 
-        new public string Name
-        {
-            get;
-            private set;
-        }
+        public int AC { get; private set; }
 
-        new public int AC
-        {
-            get;
-            private set;
-        }
+        public int Strength { get; private set; }
 
-        public Goblin()
-        {
-            Name = String.Format("Goblin");
-            AC = 15;
+        public int Dexterity { get; private set; }
 
-        }
+        public int Constitution { get; private set; }
+
+        public int Intelligence{ get; private set;}
+
+        public int Wisdom{ get; private set;}
+
+        public int Charisma{ get; private set;}
+
+        public int StrengthMod{ get; private set;}
+
+        public int DexterityMod{ get; private set;}
+
+        public int ConstitutionMod { get; private set; }
+
+        public int IntelligenceMod { get; private set; }
+
+        public int WisdomMod { get; private set; }
+
+        public int CharismaMod { get; private set; }
     }
 }
